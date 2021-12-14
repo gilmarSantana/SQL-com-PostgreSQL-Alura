e24f0781680f9c15c96f1f30b214c4a11f720d46
gilmarSantana <gilmar.livecont@gmail.com>
2021-12-14 17:47:25 -0300

    Aula 03

--- aula-02.sql
+++ aula-02.sql
@@ -4,9 +4,10 @@
 INSERT INTO aluno (nome, cpf, observacao, idade, dinheiro, altura, ativo, data_nascimento, hora_aula, matriculado_em) VALUES ('Gilmar Santana Lins', '33333333333', 'We dont have coments for this task, please return after and check if there is some coment here.',24,100.54,1.81,TRUE,'1997-04-12','17:00:00','2021-12-13 17:00:00');
 
 -- Atualizando dados nas tabelas
-UPDATE aluno SET cpf = '44444444444', observacao = 'There was a change in the CPF' WHERE id = 1
+UPDATE aluno SET cpf = '44444444444', observacao = 'There was a change in the CPF' WHERE id = 1;
 
 -- Deletando registros da tabela
-DELETE FROM aluno WHERE id = 1
+DELETE FROM aluno WHERE id = 1;
 
+-- Lendo dados da tabela
 SELECT * FROM aluno;
---
+++ aula-03.sql
@@ -0,0 +1,25 @@
+﻿-- Podemos colocar apelidos nos campos quando fizermos SELECT
+SELECT 
+	nome as "Nome Completo",
+	observacao as "Última alteração",
+	dinheiro as Capital
+FROM ALUNO;
+
+-- Trabalhando com filtros no SELECT 
+select * from aluno where nome = 'Fabio da Silva';
+select * from aluno where nome <> 'Lucas Melo';
+select * from aluno where nome like '_ucas Melo'; -- Qualquer caracter no lugar de _ 'underline'
+select * from aluno where nome not like '_ucas Melo';
+select * from aluno where nome like 'L%'; -- Começa com:
+select * from aluno where nome like '%a'; -- Termina com:
+select * from aluno where nome like '%va%'; -- Contém:
+select * from aluno where nome like '%Fa%va%'; -- Contém:
+
+select * from aluno where cpf is null;
+select * from aluno where cpf is not null;
+
+select * from aluno where idade >= 24; -- maior, menor, maior ou igual, menor ou igual
+select * FROM aluno where idade between 10 and 25;
+
+select * from aluno where idade >= 18 and cpf is not null;
+select * from aluno where idade >= 18 or dinheiro >= 100;
