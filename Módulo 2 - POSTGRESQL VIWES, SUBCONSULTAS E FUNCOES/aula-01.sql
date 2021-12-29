e79f24df82a6c77c4e7be9477400991eb86635c1
gilmarSantana <gilmar.livecont@gmail.com>
2021-12-29 13:47:32 -0300

    Módulo 3 aula 1 e 2

---
+++ Módulo 2 - POSTGRESQL VIWES, SUBCONSULTAS E FUNCOES/aula-01.sql
@@ -0,0 +1,18 @@
+﻿-- Revisão do módulo anterior
+create table aluno (
+	id serial primary key,
+	primeiro_nome varchar(255) not null,
+	ultimo_nome varchar(255) not null,
+	data_nascimento date not null
+);
+
+create table curso (
+	id serial primary key,
+	nome varchar(255) not null
+);
+
+create table aluno_curso (
+	aluno_id integer not null references aluno(id),
+	curso_id integer not null references curso(id),
+	primary key(aluno_id, curso_id)
+);
---
+++ Módulo 2 - POSTGRESQL VIWES, SUBCONSULTAS E FUNCOES/aula-02.sql
@@ -0,0 +1,38 @@
+﻿create table categoria (
+	id serial primary key,
+	nome varchar(255) not null
+);
+
+
+alter table curso
+add column categoria_id integer not null references categoria(id);
+
+insert into categoria (nome) values('backEnd');
+insert into categoria (nome) values('frontEnd');
+insert into categoria (nome) values('dataBase');
+insert into categoria (nome) values('UX');
+
+select * from aluno_curso;
+
+insert into aluno (primeiro_nome, ultimo_nome, data_nascimento) values ('Gilmar', 'Santana Lins', '1997-04-12');
+insert into aluno (primeiro_nome, ultimo_nome, data_nascimento) values ('Renato', 'Ferreira dos Santos', '1986-10-08');
+insert into aluno (primeiro_nome, ultimo_nome, data_nascimento) values ('Augusto', 'Torres Serafim', '2000-06-14');
+insert into aluno (primeiro_nome, ultimo_nome, data_nascimento) values ('Rodrigo', 'Ferreira Desan', '1987-09-24');
+
+insert into curso (nome, categoria_id) values ('PHP', 1);
+insert into curso (nome, categoria_id) values ('Python', 1);
+insert into curso (nome, categoria_id) values ('CSS', 2);
+insert into curso (nome, categoria_id) values ('SaSS', 2);
+insert into curso (nome, categoria_id) values ('PostgreSql', 3);
+insert into curso (nome, categoria_id) values ('MongoDb', 3);
+insert into curso (nome, categoria_id) values ('User Experience Essentials', 4);
+insert into curso (nome, categoria_id) values ('User Experience Professional', 4);
+
+insert into aluno_curso values (1, 1);
+insert into aluno_curso values (2, 3);
+insert into aluno_curso values (3, 5);
+insert into aluno_curso values (4, 7);
+insert into aluno_curso values (1, 2);
+insert into aluno_curso values (2, 4);
+insert into aluno_curso values (3, 6);
+insert into aluno_curso values (4, 8);
