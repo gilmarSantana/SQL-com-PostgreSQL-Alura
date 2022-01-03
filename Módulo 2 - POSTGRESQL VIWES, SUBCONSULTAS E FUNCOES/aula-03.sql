84b5e118c8c3251e719e9094685015b305111ae9
gilmarSantana <gilmar.livecont@gmail.com>
2022-01-03 07:55:38 -0300

    aula 03

---
+++ Módulo 2 - POSTGRESQL VIWES, SUBCONSULTAS E FUNCOES/aula-03.sql
@@ -0,0 +1,11 @@
+﻿select primeiro_nome, ultimo_nome, count(aluno_curso.curso_id) as contagem_cursos from aluno
+	inner join aluno_curso on aluno.id = aluno_curso.aluno_id
+group by 1, 2
+order by contagem_cursos desc
+limit 1;
+
+select  curso.nome, count(aluno_curso.aluno_id) as curso_mais_requisitado from curso
+	INNER JOIN aluno_curso on curso.id = aluno_curso.curso_id
+group by 1
+order by curso.nome;
+	
